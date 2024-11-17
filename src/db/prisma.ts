b258aa3df2d28c5d1546ce8401a3fad5f1244af8
Swapnil <swapnilbajpai@connectclub.in>
2024-11-17 16:02:50 +0530

    Prisma fix attempt

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -5,15 +5,14 @@ declare global {
     var cachedPrisma: PrismaClient
 }
 
-let prisma: PrismaClient
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-} else {
-    if (!global.cachedPrisma) {
-        global.cachedPrisma = new PrismaClient()
-    }
-
-    prisma = global.cachedPrisma
+// if (process.env.NODE_ENV === 'production') {
+//     prisma = new PrismaClient()
+// } else {
+if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
 }
+
+const prisma: PrismaClient = global.cachedPrisma
+// }
 
 export const db = prisma;
